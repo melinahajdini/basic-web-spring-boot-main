37e2ee865cc094ebcd8d8493a75c9a479c2986df
melinahajdini <61281674+melinahajdini@users.noreply.github.com>
2021-11-07 18:33:47 +0100

    Add necessary files

---
+++ src/main/java/com/edon/basic/web/controller/FitnessController.java
@@ -0,0 +1,8 @@
+package com.edon.basic.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FitnessController {
+
+}
---
+++ src/main/java/com/edon/basic/web/model/FitnessUser.java
@@ -0,0 +1,51 @@
+package com.edon.basic.web.model;
+
+public class FitnessUser {
+
+    private String name;
+    private String lastName;
+    private int age;
+    private String gjinia;
+    private boolean isActive;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGjinia() {
+        return gjinia;
+    }
+
+    public void setGjinia(String gjinia) {
+        this.gjinia = gjinia;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+}
