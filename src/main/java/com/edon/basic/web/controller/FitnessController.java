2c59443ef79df0bbfeead88792fb0f2f94364422
melinahajdini <61281674+melinahajdini@users.noreply.github.com>
2021-11-07 18:54:30 +0100

    Add post mapping

--- src/main/java/com/edon/basic/web/controller/FitnessController.java
+++ src/main/java/com/edon/basic/web/controller/FitnessController.java
@@ -1,8 +1,24 @@
 package com.edon.basic.web.controller;
 
+import com.edon.basic.web.model.FitnessUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FitnessController {
 
+    @GetMapping("/addfitnessuser")
+    public String getFitness(Model model) {
+        model.addAttribute("fitnessUser", new FitnessUser());
+        return "FitnessApp";
+
+    }
+    @PostMapping("/viewfitnessusers")
+    public String createUser(@ModelAttribute FitnessUser fitnessUser, Model model) {
+        model.addAttribute("fitnessUser", fitnessUser);
+        return "ViewFitnessUsers";
+    }
 }
